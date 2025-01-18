bfa9d1a13e8948b335d5a4390780f3d3c4f5dc9a
byeoon <47872200+byeoon@users.noreply.github.com>
2025-01-18 02:09:26 +0000

    added tailwindcss support, no longer using CDN!

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./public/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: true, // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "black", // name of one of the included themes for dark mode
+  },
+}
